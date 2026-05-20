0c78ccb58c1146f084309542972dd96ab134a32e
Maharo-Andri <mhrmaharo@gmail.com>
2026-05-20 18:43:56 +0300

    Ajout Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    agent any
+    stages {
+        stage('clone and clean repo') {
+            steps {
+                bat "if exist demoic rmdir /s /q demoic"
+                bat "git clone https://github.com/heritsilavo/demoic"
+                bat "mvn clean -f DemoIC"
+            }
+        }
+        stage('Test') {
+            steps {
+                bat "mvn test -f DemoIC"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                bat "mvn package -f DemoIC"
+                bat "mvn deploy -f DemoIC -s C:\\Users\\Maharo\\.m2\\settings.xml"
+                bat "mvn sonar:sonar -f DemoIC -Dsonar.token=squ_3b44443669924d1f455d6a358434eb167d62e7b0"
+            }
+        }
+    }
+}
